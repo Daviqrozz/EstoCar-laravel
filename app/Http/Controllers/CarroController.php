653c50181b3f3b84de1735dcc942077474bbe91e
Davi <daviqrozz2@gmail.com>
2025-09-07 16:29:12 -0300

    feat: criar CarroController

---
+++ app/Http/Controllers/CarroController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carro;
+use Illuminate\Http\Request;
+
+class CarroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cars = Carro::all();
+
+        return response()->json([
+            'Carros' => $cars
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
